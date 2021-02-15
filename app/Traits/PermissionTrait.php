660ffd7ec85d89ff69b46c88eb812ede148c2216
odik91 <ali.shoddiqien@gmail.com>
2021-02-15 16:09:55 +0700

    adding traits for permissions

---
+++ app/Traits/PermissionTrait.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Traits;
+
+use Illuminate\Support\Facades\Route;
+
+/**
+ * Permission traits
+ */
+
+trait PermissionTrait {
+  public function hasPermission() {
+    // 
+  }
+}
